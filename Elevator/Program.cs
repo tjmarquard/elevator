e799fe13a88832ad7a87c563f5950f8cd3fda3a9
Tim <tjmarquard@gmail.com>
2021-06-21 21:47:29 -0500

    scaffold with Hello World

---
+++ Elevator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
