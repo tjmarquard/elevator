227a970512a4a5736686565928b42d6d96ffb9e7
Tim <tjmarquard@gmail.com>
2021-06-22 21:32:54 -0500

    weight limits

--- Elevator.Tests/CarTests.cs
+++ Elevator.Tests/CarTests.cs
@@ -8,7 +8,7 @@ namespace Elevator.Test
         [Fact]
         public void ElevatorShouldStartWithAnEmptyFloorQueue()
         {
-            var subjectUnderTest = new Car();
+            var subjectUnderTest = new Car(5);
 
             Assert.Empty(subjectUnderTest.FloorQueue);
         }
@@ -16,10 +16,70 @@ namespace Elevator.Test
         [Fact]
         public void CarShouldStartStaionary()
         {
-            var subjectUnderTest = new Car();
+            var subjectUnderTest = new Car(5);
 
             Assert.Equal(DirectionOfTravel.STAIONARY, subjectUnderTest.DirectionOfTravel);
             Assert.False(subjectUnderTest.Moving);
+        }
+
+        [Fact]
+        public void CarShouldHaveFiveFloors()
+        {
+            var subjectUnderTest = new Car(5);
+
+            Assert.Collection<int>(subjectUnderTest.Floors, 
+                floor => Assert.Equal(1, floor),
+                floor => Assert.Equal(2, floor),
+                floor => Assert.Equal(3, floor),
+                floor => Assert.Equal(4, floor),
+                floor => Assert.Equal(5, floor)
+                );
+        }
+
+        [Fact]
+        public void CarShouldStartOnFirstFloor()
+        {
+            var subjectUnderTest = new Car(5);
+
+            Assert.Equal(1, subjectUnderTest.CurrentFloor);
+        }
+
+        [Fact]
+        public void CarHasAMaxWeightLimitOf2000()
+        {
+            var subjectUnderTest = new Car(5);
+
+            Assert.Equal(2000, subjectUnderTest.MaxWeightLimit);
+        }
+
+        [Fact]
+        public void CarIsUnderMaxWeightLimit()
+        {
+            var subjectUnderTest = new Car(5);
+
+            subjectUnderTest.Weight = subjectUnderTest.MaxWeightLimit - 1;
+
+            Assert.True(subjectUnderTest.IsUnderMaxWeightLimit);
+        }
+
+        [Fact]
+        public void CarIsOverMaxWeightLimit()
+        {
+            var subjectUnderTest = new Car(5);
+
+            subjectUnderTest.Weight = subjectUnderTest.MaxWeightLimit + 1;
+
+            Assert.False(subjectUnderTest.IsUnderMaxWeightLimit);
+        }
+
+        [Fact]
+        public void CarIsAdMaxWeightLimit()
+        {
+            var subjectUnderTest = new Car(5);
+
+            subjectUnderTest.Weight = subjectUnderTest.MaxWeightLimit;
+
+            Assert.True(subjectUnderTest.IsUnderMaxWeightLimit);
         }
     }
 }
--- Elevator/Car.cs
+++ Elevator/Car.cs
@@ -12,10 +12,20 @@ namespace Elevator
         public List<int> FloorQueue { get; set; }
         public bool Moving { get; set; } = false;
 
-        public Car()
+        public List<int> Floors { get; private set; }
+        public int CurrentFloor { get; set; } = 1;
+        public int MaxWeightLimit { get; } = 2000;
+        public int Weight { get; set; }
+        public bool IsUnderMaxWeightLimit
+        {
+            get => Weight <= MaxWeightLimit;
+        }
+
+        public Car(int numberOfFloors)
         {
             FloorQueue = new List<int>();
             DirectionOfTravel = DirectionOfTravel.STAIONARY;
+            Floors = Enumerable.Range(1, numberOfFloors).ToList();
         }
     }
 }
