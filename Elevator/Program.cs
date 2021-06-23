36d733a90c06e5116c8399ede2af426a2a706359
Tim <tjmarquard@gmail.com>
2021-06-22 21:51:02 -0500

    started console inputs

--- Elevator/Program.cs
+++ Elevator/Program.cs
@@ -6,7 +6,27 @@ namespace Elevator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var numberOfFloors = PromptHowManyFloors();
+
+            int floors;
+
+            while (!Int32.TryParse(numberOfFloors, out floors))
+            {
+                Console.WriteLine("An invalid value was entered");
+                numberOfFloors = PromptHowManyFloors();
+            }
+
+            var car = new Car(floors);
+            Console.WriteLine($"It serves {floors} floors.");
+
+
+        }
+
+        private static string PromptHowManyFloors()
+        {
+            Console.WriteLine("How many floors does this elevator service?");
+
+            return Console.ReadLine();
         }
     }
 }
