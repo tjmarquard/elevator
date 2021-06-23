29e88d90bbcc7d5cea7c5b20be5248a3de284b42
Tim <tjmarquard@gmail.com>
2021-06-22 21:01:00 -0500

    start building elevator car

---
+++ Elevator.Tests/CarTests.cs
@@ -0,0 +1,24 @@
+using System;
+using Xunit;
+
+namespace Elevator.Test
+{
+    public class CarTests
+    {
+        [Fact]
+        public void ElevatorShouldStartWithAnEmptyFloorQueue()
+        {
+            var subjectUnderTest = new Car();
+
+            Assert.Empty(subjectUnderTest.FloorQueue);
+        }
+
+        [Fact]
+        public void CarShouldStartStaionary()
+        {
+            var subjectUnderTest = new Car();
+
+            Assert.Equal(DirectionOfTravel.STAIONARY, subjectUnderTest.DirectionOfTravel);
+        }
+    }
+}
---
+++ Elevator/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elevator
+{
+    public class Car
+    {
+        public DirectionOfTravel DirectionOfTravel { get; set; }
+        public List<int> FloorQueue { get; set; }
+
+        public Car()
+        {
+            FloorQueue = new List<int>();
+            DirectionOfTravel = DirectionOfTravel.STAIONARY;
+        }
+    }
+}
